448218816a4d9a72e868d4a76327152864066b4d
Cleardragonf <chrism339@yahoo.com>
2018-10-01 18:55:33 -0500

    • Updated to Minecraft 1.12.2 sponge API 7.1.0

---
+++ ExplosionGuard/src/main/java/me/cleardragonf/eg/ConfigurationManager.java
@@ -0,0 +1,77 @@
+package me.cleardragonf.eg;
+
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ConfigurationManager {
+
+    private static ConfigurationManager instance = new ConfigurationManager();
+
+    public static ConfigurationManager getInstance() {
+        return instance;
+    }
+
+    private ConfigurationLoader<CommentedConfigurationNode> configLoader;
+    private CommentedConfigurationNode config;
+
+    public void setup(File configFile, ConfigurationLoader<CommentedConfigurationNode> configLoader) {
+        this.configLoader = configLoader;
+
+        if (!configFile.exists()) {
+            try {
+                configFile.createNewFile();
+                loadConfig();
+                config.getNode("Explosions").setComment("Disable or Enable These Explosion Types? True = No Explosions || False = Explosions");
+                config.getNode("Explosions", "TNT").setValue("true");
+                config.getNode("Explosions", "Creeper").setValue("true");
+                config.getNode("Explosions", "Ghast").setValue("true");
+                config.getNode("Undo?").setComment("Would you like any damage to be undone?");
+                config.getNode("Undo?", "Creeper", "Would you like this to Rollback?").setValue("yes");
+                config.getNode("Undo?", "Creeper", "Time to Heal?").setComment("in Seconds").setValue("5");
+                config.getNode("Undo?", "TNT", "Would you like this to Rollback?").setValue("yes");
+                config.getNode("Undo?", "TNT", "Time to Heal?").setComment("in Seconds").setValue("10");
+                config.getNode("Undo?", "Ghast", "Would you like this to Rollback?").setValue("yes");
+                config.getNode("Undo?", "Ghast", "Time to Heal?").setValue("10");
+                config.getNode("Items", "TNT", "Drop").setValue("false");
+                config.getNode("Items", "Ghast", "Drop").setValue("false");
+                config.getNode("Items", "Creeper", "Drop").setValue("false");
+                saveConfig();
+            }
+
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        else {
+            loadConfig();
+        }
+    }
+
+    public CommentedConfigurationNode getConfig() {
+        return config;
+    }
+
+    public void saveConfig() {
+        try {
+            configLoader.save(config);
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadConfig() {
+        try {
+            config = configLoader.load();
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ ExplosionGuard/src/main/java/me/cleardragonf/eg/ExplosionGuard.java
@@ -0,0 +1,161 @@
+package me.cleardragonf.eg;
+
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.config.DefaultConfig;
+import org.spongepowered.api.entity.explosive.PrimedTNT;
+import org.spongepowered.api.entity.living.monster.Creeper;
+import org.spongepowered.api.entity.living.monster.Ghast;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.Order;
+import org.spongepowered.api.event.game.state.GameStartingServerEvent;
+import org.spongepowered.api.event.item.inventory.DropItemEvent;
+import org.spongepowered.api.event.world.ExplosionEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+import com.google.inject.Inject;
+
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+
+@Plugin(id = "explosionguard", name = "ExplosionGuard", version = "Beta.1.0")
+
+public class ExplosionGuard {
+
+    @Inject
+    private Logger logger;
+
+    @Inject
+    private void setLogger(Logger logger){
+        this.logger = logger;
+    }
+    public Logger getLogger(){
+        return logger;
+    }
+
+    @Listener
+    public void enable(GameStartingServerEvent event)
+    {
+        ConfigurationManager.getInstance().setup(configFile, configManager);
+    }
+
+    @Inject
+    @DefaultConfig(sharedRoot = false)
+    private File configFile;
+
+    @Inject
+    @DefaultConfig(sharedRoot = false)
+    ConfigurationLoader<CommentedConfigurationNode> configManager;
+
+    @Listener
+    public void onExplosionEvent(ExplosionEvent.Pre event) {
+            /*--
+            getLogger().info(event.getCause().toString());
+            ---*/
+        if (event.getCause().first(Creeper.class).isPresent()) {
+            if (ConfigurationManager.getInstance().getConfig().getNode("Explosions", "Creeper").getString().equalsIgnoreCase("true")){
+                event.setCancelled(true);
+            }
+            if (ConfigurationManager.getInstance().getConfig().getNode("Explosions", "Creeper").getString().equalsIgnoreCase("false")){
+                event.setCancelled(false);
+            }
+        }
+        if (event.getCause().first(PrimedTNT.class).isPresent()){
+            if (ConfigurationManager.getInstance().getConfig().getNode("Explosions", "TNT").getString().equalsIgnoreCase("true")){
+                event.setCancelled(true);
+            }
+            if (ConfigurationManager.getInstance().getConfig().getNode("Explosions", "TNT").getString().equalsIgnoreCase("false")){
+                event.setCancelled(false);
+
+            }
+        }
+        if (event.getCause().first(Ghast.class).isPresent()){
+            if (ConfigurationManager.getInstance().getConfig().getNode("Explosions", "Ghast").getString().equalsIgnoreCase("true")){
+                event.setCancelled(true);
+            }
+            if (ConfigurationManager.getInstance().getConfig().getNode("Explosions", "Ghast").getString().equalsIgnoreCase("false")){
+                event.setCancelled(false);
+
+            }
+        }
+        //this is here for logging what caused the explosiong so it can be logged
+        //*getLogger().info(event.getCause().toString());
+
+    }
+
+    /*---This operates the healing time and operations---*/
+
+    @Listener(order = Order.LAST)
+
+    public void onExplode(final ExplosionEvent.Detonate bang){
+        if (bang.getCause().first(PrimedTNT.class).isPresent()) {
+            if (ConfigurationManager.getInstance().getConfig().getNode("Explosions", "TNT").getString().equalsIgnoreCase("false")){
+                if (ConfigurationManager.getInstance().getConfig().getNode("Undo?", "TNT", "Would you like this to Rollback?").getString().equalsIgnoreCase("yes")){
+                    final List<Location<World>> transactions = bang.getAffectedLocations();
+                    final Mend mend = new Mend(transactions);
+                    Sponge.getScheduler().createTaskBuilder()
+                            .delay(ConfigurationManager.getInstance().getConfig().getNode("Undo?", "TNT", "Time to Heal?").getLong(), TimeUnit.SECONDS)
+                            .name("Explosion Repair Task")
+                            .execute(mend::heal)
+                            .submit(this);
+                }
+            }
+        }
+        if (bang.getCause().first(Creeper.class).isPresent()) {
+            if (ConfigurationManager.getInstance().getConfig().getNode("Explosions", "Creeper").getString().equalsIgnoreCase("false")){
+                if (ConfigurationManager.getInstance().getConfig().getNode("Undo?", "Creeper", "Would you like this to Rollback?").getString().equalsIgnoreCase("yes")){
+                    bang.getAffectedLocations();
+                    final List<Location<World>> transactions = bang.getAffectedLocations();
+                    final Mend mend = new Mend(transactions);
+                    Sponge.getGame().getScheduler().createTaskBuilder()
+                            .delay(15, TimeUnit.SECONDS)
+                            .name("Explosion Repair Task")
+                            .execute(mend::heal)
+                            .submit(this);
+                }
+            }
+        }
+        if (bang.getCause().first(Ghast.class).isPresent()) {
+            if (ConfigurationManager.getInstance().getConfig().getNode("Explosions", "Ghast").getString().equalsIgnoreCase("false")){
+                if (ConfigurationManager.getInstance().getConfig().getNode("Undo?", "Undo_Ghast's_Explosion").getString().equalsIgnoreCase("yes")){
+                    bang.getAffectedLocations();
+                    final List<Location<World>> transactions = bang.getAffectedLocations();
+                    final Mend mend = new Mend(transactions);
+                    Sponge.getScheduler().createTaskBuilder()
+                            .delay(ConfigurationManager.getInstance().getConfig().getNode("Undo?", "Ghast_Rollback_Time").getLong(), TimeUnit.SECONDS)
+                            .name("Explosion Repair Task")
+                            .execute(mend::heal)
+                            .submit(this);
+                }
+            }
+        }
+    }
+
+    /*---This here manages the itemDrops from Explosions---*/
+    @Listener
+    public void itemDrop(DropItemEvent.Destruct itemDrop){
+        if (itemDrop.getCause().first(Creeper.class).isPresent()) {
+            if (ConfigurationManager.getInstance().getConfig().getNode("Items", "Creeper", "Drop").getString().equalsIgnoreCase("false")){
+                itemDrop.setCancelled(true);
+            }
+        }
+        if (itemDrop.getCause().first(PrimedTNT.class).isPresent()){
+            if (ConfigurationManager.getInstance().getConfig().getNode("Items", "TNT", "Drop").getString().equalsIgnoreCase("false")){
+                itemDrop.setCancelled(true);
+            }
+        }
+        if (itemDrop.getCause().first(Ghast.class).isPresent()){
+            if (ConfigurationManager.getInstance().getConfig().getNode("Items", "Ghast", "Drop").getString().equalsIgnoreCase("false")){
+                itemDrop.setCancelled(true);
+            }
+        }
+    }
+
+}
