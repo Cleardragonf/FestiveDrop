1e5b68b6a5c5e460c8f80d133b893fcddca86e26
Cleardragonf <chrism339@yahoo.com>
2018-10-21 15:08:00 -0500

    testing

---
+++ asdf/src/main/java/asdf/asdf/Asdf.java
@@ -0,0 +1,21 @@
+package asdf.asdf;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(
+        id = "asdf",
+        name = "Asdf"
+)
+public class Asdf {
+
+    @Inject
+    private Logger logger;
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+    }
+}
